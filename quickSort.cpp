e6e5894e000a27bff322bc6bb27268ee6e8e3f8d
Pham Duy Truong <daikooh1234@gmail.com>
2022-07-07 07:39:19 +0700

    da them comment vao file

--- quickSort.cpp
+++ quickSort.cpp
@@ -14,18 +14,23 @@ void nhapMang(vector<int> &a,int &n)
 void quickSort(vector<int> &a,int left,int right)
 {
     int i,j,x;
-    x=a[(left+right)/2];
-    i=left,j=right;
+    //4 8 1 23 21 19 3
+    x=a[(left+right)/2];//x=23
+    i=left,j=right;//i=0,j=7
     do
     {
         while(a[i]<x) i++;
         while(a[j]>x) j--;
+        //i=3,j=7
+        //i=7,j=6
         if(i<=j)
         {
             swap(a[i],a[j]);
             i++;
             j--;
         }
+        //4 8 1 3 21 19 23
+        //i=4,j=6
     }
     while(i<=j);
     if(left<j) quickSort(a,left,j);

9b2c31b7a372df1225510204e423580ae3b555c7
Pham Duy Truong <daikooh1234@gmail.com>
2022-07-29 09:49:30 +0700

    Da them moi

--- linkList1.cpp
+++ linkList1.cpp
@@ -53,8 +53,9 @@ void insertLast(LIST &l,Node *p)
 }
 void nhapDSLK(LIST &l,int &n)
 {
-    cout<<"Nhap so luong phan tu: ";cin>>n;
-    for(int i=1;i<=n;i++)
+    cout<<"Nhap so luong phan tu: ";
+    cin>>n;
+    for(int i=1; i<=n; i++)
     {
         int b;
         cout<<"Nhap gia tri node: ";
@@ -69,19 +70,89 @@ void xuatNode(Node *p)
 }
 void xuatDSLK(LIST l)
 {
-    Node *p=l.head;
     cout<<setw(10)<<"Gia tri"<<endl;
-    while(p!=NULL)
+    for(Node *k=l.head;k!=NULL;k=k->next)
     {
-        xuatNode(p);
-        p=p->next;
+        xuatNode(k);
+    }
+}
+void insertWithNode(LIST &l,Node *p)
+{
+    int x;
+    xuatDSLK(l);
+    cout<<"Nhap gia tri node q: ";
+    cin>>x;
+    Node *q=createNode(x);
+    if(l.head->next==NULL&&q->data==l.head->data)
+    {
+        insertLast(l,p);
+    }
+    else
+    {
+        for(Node *k=l.head;k!=NULL;k=k->next)
+        {
+            if(q->data==k->data)
+            {
+                Node *g=k->next;
+                p->next=g;
+                k->next=p;
+            }
+        }
     }
 }
 int main()
 {
+    int chon;
     int n;
     LIST l;
     initList(l);
-    nhapDSLK(l,n);
-    xuatDSLK(l);
+    do
+    {
+        system("cls");
+        cout<<"===================MENU================="<<endl;
+        cout<<"|| 1. Them phan tu vao dau danh sach  ||"<<endl;
+        cout<<"|| 2. Them phan tu vao cuoi danh sach ||"<<endl;
+        cout<<"|| 3. Them vao sau node q             ||"<<endl;
+        cout<<"|| 4. Hien danh sach lien ket         ||"<<endl;
+        cout<<"|| 0. Thoat                           ||"<<endl;
+        cout<<"========================================"<<endl;
+        {
+            cout<<"Nhap lua chon cua ban: ";
+            cin>>chon;
+            switch(chon)
+            {
+            case 1:
+                {
+                    int a;
+                    cout<<"Nhap gia tri node: ";cin>>a;
+                    Node *p=createNode(a);
+                    insertHead(l,p);
+                    break;
+                }
+            case 2:
+                {
+                    int a;
+                    cout<<"Nhap gia tri node: ";cin>>a;
+                    Node *p=createNode(a);
+                    insertLast(l,p);
+                    break;
+                }
+            case 3:
+                {
+                    int x;
+                    cout<<"Nhap gia tri node p: ";
+                    cin>>x;
+                    Node *p=createNode(x);
+                    insertWithNode(l,p);
+                    break;
+                }
+            case 4:
+                {
+                    xuatDSLK(l);
+                     break;
+                }
+            }
+        }
+        system("pause");
+    }while(chon!=0);
 }
